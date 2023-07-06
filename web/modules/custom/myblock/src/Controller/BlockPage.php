83e0315756cf10d70a6771c1b191958856379889
Arijit Acharya <arijit.acharya@innoraft.com>
2023-07-07 01:40:24 +0530

    FT2023-343: Addition of all the files for the custom block module.

---
+++ web/modules/custom/myblock/src/Controller/BlockPage.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\myblock\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * This implements a class for,
+ *  creating a simple page where a block will be implemented.
+ */
+class BlockPage extends ControllerBase {
+
+  /**
+   * @return array
+   * Implements basic page written as hello world.
+   */
+  function pageDisplay() {
+   return[
+    '#type'=>'markup',
+    '#markup' => $this->t('Hello World!')
+   ];
+  }
+
+}
+
+?>
