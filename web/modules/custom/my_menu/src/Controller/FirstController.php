e70a523664b843cb862b9724c7ba0f3175526fc3
Arijit Acharya <arijit.acharya@innoraft.com>
2023-08-11 02:11:11 +0530

    FT2023-347: Fixing of the coding standards

--- web/modules/custom/my_menu/src/Controller/FirstController.php
+++ web/modules/custom/my_menu/src/Controller/FirstController.php
@@ -4,38 +4,63 @@ namespace Drupal\my_menu\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
+/**
+ * Controller for handling menu-related pages.
+ */
 class FirstController extends ControllerBase {
 
+  /**
+   * Callback for the normal menu page.
+   *
+   * @return array
+   *   A render array containing the markup for the normal menu page.
+   */
   public function normalMenu() {
-    return[
-      '#type'=>'markup',
-      '#markup'=>'This page is for menu.'
+    return [
+      '#type' => 'markup',
+      '#markup' => 'This page is for menu.',
     ];
   }
 
+  /**
+   * Callback for a local task menu page.
+   *
+   * @return array
+   *   A render array containing the markup for the local task menu page.
+   */
   public function localTask() {
     return [
       '#type' => 'markup',
       '#markup' => 'Used for creating a local task Menu.These are tabs.
        Thesed are local to the displayed page.
-       These are mostly used in administrative pages.'
+       These are mostly used in administrative pages.',
     ];
   }
 
+  /**
+   * Callback for a local action menu page.
+   *
+   * @return array
+   *   A render array containing the markup for the local action menu page.
+   */
   public function localAction() {
     return [
       '#type' => 'markup',
-      '#markup' => 'These are responsible to conduct a action rather than display an information. '
+      '#markup' => 'These are responsible to conduct a action rather than display an information. ',
     ];
   }
 
+  /**
+   * Callback for a contextual menu page.
+   *
+   * @return array
+   *   A render array containing the markup for the contextual menu page.
+   */
   public function contextualMenu() {
-    return[
+    return [
       '#type' => 'markup',
-      '#markup'=> 'These are the fields present in the edit/Configuration option.'
+      '#markup' => 'These are the fields present in the edit/Configuration option.',
     ];
   }
 
-  
-  
 }
--- web/modules/custom/my_menu/src/Form/CustomConfigForm.php
+++ web/modules/custom/my_menu/src/Form/CustomConfigForm.php
@@ -1,43 +1,60 @@
 <?php
 
-namespace  Drupal\my_menu\Form;
+namespace Drupal\my_menu\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Entity\EntityInterface;
 
-class CustomConfigForm extends ConfigFormBase{
+/**
+ * Provides a form for custom configuration settings related to this module.
+ */
+class CustomConfigForm extends ConfigFormBase {
 
   const CONFIGNAME = 'my_menu.settings';
 
+  /**
+   * {@inheritdoc}
+   */
   public function getFormId() {
     return 'my_menu_settings';
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function getEditableConfigNames() {
     return [
-      static::CONFIGNAME
+      static::CONFIGNAME,
     ];
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config(static::CONFIGNAME);
     $form['budget'] = [
       '#type' => 'number',
       '#title' => 'Budget',
       '#suffix' => 'cr',
-      '#default_value'=> $config->get('budget')
+      '#default_value' => $config->get('budget'),
     ];
 
     return parent::buildForm($form, $form_state);
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function validateForm(array &$form, FormStateInterface $form_state) {
     if (preg_match("/^[0-9]+$/", $form_state->getValue('budget')) == FALSE) {
       $form_state->setErrorByName('budget', $this->t('Only numbers are allowed'));
     }
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $config = $this->config(static::CONFIGNAME);
     $config->set('budget', $form_state->getValue('budget'));
